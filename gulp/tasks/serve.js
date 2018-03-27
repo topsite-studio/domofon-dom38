852fa70c00faea58d584724e4dc39141ee35ad24
Alexander B <r3disrussian@gmail.com>
2018-03-27 17:59:40 +0800

    Добавил в зависимости HTTP/2 Раз я включил в browsersync режим https, то могу позволить себе и http2

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,13 +1,14 @@
-module.exports = function() {
-	$.gulp.task('serve', function() {
-		$.browserSync.init({
-			server: 'build',
-			https: true,
-	//			proxy: "http://domofon/build/",
-			// reloadDelay: 2000,
-			notify: false,
-			//tunnel: true,
-			//tunnel: "project", //Demonstration page: http://projectmane.localtunnel.me
-		});
-	});
-};
+module.exports = function () {
+  $.gulp.task('serve', function () {
+    $.browserSync.init({
+      server: 'build',
+      https: true,
+      httpModule: 'http2',
+      //			proxy: "http://domofon/build/",
+      // reloadDelay: 2000,
+      notify: false
+      // tunnel: true,
+      // tunnel: "project", //Demonstration page: http://projectmane.localtunnel.me
+    })
+  })
+}
