230eb4e8113fd36821137fd0f33c7d3111850549
Alexander B <r3disrussian@gmail.com>
2018-04-05 12:17:54 +0800

    Теперь все JS файлы в dev/js конкатенируются в один файл common.js

---
+++ dev/js/keystores.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict'
+
+  if (document.querySelector('.page--keyretailers')) {
+    console.log('key retailers map')
+    document.addEventListener('DOMContentLoaded', keyretail)
+  }
+
+  function keyretail () {
+    console.log('kakakak!')
+  }
+}())
