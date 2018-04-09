67b9476af83753ed84b8eb6b0e8ae8d754afe00b
Alexander B <r3disrussian@gmail.com>
2018-04-09 14:33:19 +0800

    Теперь на телефонах offset якоря составляет 50px

--- dev/js/anchors.js
+++ dev/js/anchors.js
@@ -5,7 +5,7 @@
     e.preventDefault()
     var target = $($(this).attr('href'))
     var animationSpeed = 750
-    var offsetTop = document.documentElement.clientHeight / 4
+    var offsetTop = (window.matchMedia('(min-width: 641px)').matches) ? document.documentElement.clientHeight / 4 : 50
     var destination = target.offset().top - offsetTop
 
     $('html,body').animate({ scrollTop: destination }, animationSpeed)
