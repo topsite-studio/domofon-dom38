68709e96c3fc7e65db5a69cf845fcec03c71d62a
Alexander B <r3disrussian@gmail.com>
2018-04-10 00:34:21 +0800

    Переделал карту на контактах

---
+++ dev/js/contacts.js
@@ -0,0 +1,21 @@
+(function (ymaps) {
+  'use strict'
+
+  if (document.querySelector('.page--contacts')) document.addEventListener('DOMContentLoaded', contacts)
+
+  function contacts () {
+    var myMap
+    var mark = [52.281688, 104.311922]
+    ymaps.ready(initMap)
+    function initMap () {
+      myMap = new ymaps.Map('map', {
+        center: mark,
+        zoom: 13,
+        controls: ['zoomControl', 'geolocationControl']
+      })
+
+      var placemark = new ymaps.Placemark(mark)
+      myMap.geoObjects.add(placemark)
+    }
+  }
+})(window.ymaps)
