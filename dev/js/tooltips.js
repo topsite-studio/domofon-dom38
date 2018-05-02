c63b5fc957ff65e431388b6c9a42edcf200dd5fa
Alexander B <r3disrussian@gmail.com>
2018-05-03 00:47:43 +0800

    :bug: На телефонах при тапе на ссылку-сноску страница прокручивалась ...с некоторым шансом

--- dev/js/tooltips.js
+++ dev/js/tooltips.js
@@ -1,29 +1,36 @@
 (function ($, Tooltip) {
   'use strict'
 
-  var toolTips = $('.tooltips')
-  toolTips.each(function (index, el) {
-    // el.tabIndex = 0
+  var tooltipLinks = $('.tooltips')
+  if (tooltipLinks.length) {
+    var emptyContainer = document.createElement('div')
+    emptyContainer.className = 'tooltips-container'
+    emptyContainer.style.position = 'static'
+    emptyContainer.style.height = '0px'
+    document.body.appendChild(emptyContainer)
+  }
+  tooltipLinks.each(function (index, el) {
     var toolData = $(this).data('tooltips')
     var tooltipie = new Tooltip(el, {
       title: toolData,
       placement: 'top',
       html: true,
+      container: document.querySelector('.tooltips-container'),
       template: '<div class="tooltip" role="tooltip"><button type="button" class="tooltip-close">&times;</button><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
       popperOptions: {
         onCreate: function (event) {
           var tooltip = event.instance.popper
           var closeButton = tooltip.children[0]
-          closeButton.addEventListener('click', function (e) {
-            e.preventDefault()
+
+          closeButton.addEventListener('click', function () {
             tooltip.style.display = 'none'
           })
         }
       }
     })
-    $(el).click(function (event) {
-      console.log(event)
-      event.preventDefault()
-    })
+  })
+
+  tooltipLinks.click(function (e) {
+    e.stopPropagation()
   })
 }(window.$, window.Tooltip))
