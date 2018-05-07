809f3291c09f2a891e4627e49fab6509eb4f62a0
Alexander B <r3disrussian@gmail.com>
2018-05-07 12:40:15 +0800

    Скролл до спойлера теперь строго после его раскрытия

--- dev/js/accordion.js
+++ dev/js/accordion.js
@@ -4,18 +4,19 @@
   $('.accordion__title').click(function (e) {
     var $this = $(this)
 
-    $this.next('.accordion__content').stop(true, true).slideToggle('200')
+    $this.next('.accordion__content').stop(true, true).slideToggle('200', function () {
+      if (w.matchMedia('(max-width: 991px)').matches) {
+        var animationSpeed = 750
+        var anchorOffset = 40
+        var destination = $this.offset().top - anchorOffset
+        $('html, body').animate({ scrollTop: destination }, animationSpeed)
+      }
+    })
     $this.parent().toggleClass('accordion__item--active')
 
     $this.parent().siblings('.accordion__item').children('.accordion__content').stop(true, true).slideUp('200')
     $this.parent().siblings('.accordion__item').removeClass('accordion__item--active')
 
-    if (w.matchMedia('(max-width: 991px)').matches) {
-      var animationSpeed = 750
-      var anchorOffset = d.documentElement.clientHeight / 5
-      var destination = $this.offset().top - anchorOffset
-      $('html, body').animate({ scrollTop: destination }, animationSpeed)
-    }
     e.preventDefault()
     return false
   })
