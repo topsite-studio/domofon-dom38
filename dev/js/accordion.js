262750b58271ea76e9322b666b2699817c9837a2
Alexander B <r3disrussian@gmail.com>
2018-05-07 11:55:51 +0800

    Прокрутка до открытого спойлера на маленьких экранах (#2)

--- dev/js/accordion.js
+++ dev/js/accordion.js
@@ -1,7 +1,7 @@
 (function(w, d, $) {
   'use strict'
 
-  $('.accordion__title').click(function () {
+  $('.accordion__title').click(function (e) {
     var $this = $(this)
 
     $this.next('.accordion__content').stop(true, true).slideToggle('200')
@@ -9,5 +9,14 @@
 
     $this.parent().siblings('.accordion__item').children('.accordion__content').stop(true, true).slideUp('200')
     $this.parent().siblings('.accordion__item').removeClass('accordion__item--active')
+
+    if (w.matchMedia('(max-width: 991px)').matches) {
+      var animationSpeed = 750
+      var anchorOffset = d.documentElement.clientHeight / 5
+      var destination = $this.offset().top - anchorOffset
+      $('html, body').animate({ scrollTop: destination }, animationSpeed)
+    }
+    e.preventDefault()
+    return false
   })
 })(window, document, window.$);
