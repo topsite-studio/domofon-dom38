531fab9212f294f679b668aa6936438068dbb2ee
Alexander B <r3disrussian@gmail.com>
2018-05-17 09:55:32 +0800

    При переходе по якорю теперь появляется кнопка "Вернуться"
    #3

--- dev/js/anchors.js
+++ dev/js/anchors.js
@@ -1,15 +1,49 @@
-(function ($) {
+(function ($, $html) {
   'use strict'
 
   $('[data-anchor]').click(function (e) {
     e.preventDefault()
+
+    var source = $(this)[0]
+    var initialWindowOffset = $html.scrollTop
+
+    console.log({
+      'Исходная точка': source,
+      'initialWindowOffset': initialWindowOffset
+    })
+
     var target = $($(this).attr('href'))
     var animationSpeed = 750
-    var offsetTop = (window.matchMedia('(min-width: 641px)').matches) ? document.documentElement.clientHeight / 4 : 50
+    var offsetTop = (window.matchMedia('(min-width: 641px)').matches) ? $html.clientHeight / 4 : 50
     var destination = target.offset().top - offsetTop
 
     $('html,body').animate({ scrollTop: destination }, animationSpeed)
 
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      setTimeout(function () {
+        var backButton = document.createElement('span')
+        backButton.setAttribute('role', 'button')
+        backButton.className = 'back-button'
+        backButton.innerText = 'Вернуться'
+        backButton.style.position = 'fixed'
+        backButton.style.display = 'none'
+        backButton.addEventListener('click', function (event) {
+          $('html,body').animate({ scrollTop: initialWindowOffset }, animationSpeed)
+          source.style.backgroundColor = '#3292d2'
+          source.style.color = '#fff'
+          $(this).fadeOut(animationSpeed, function () {
+            $(this)[0].remove()
+          })
+          setTimeout(function () {
+            source.style.backgroundColor = ''
+            source.style.color = ''
+          }, 2000)
+        })
+        document.body.appendChild(backButton)
+        $(backButton).fadeIn(animationSpeed)
+      }, 500)
+    }
+
     return false
   })
-}(window.$))
+}(window.$, document.documentElement))
